ff37bd4d2c4a89808971f4ed7c0477fe0a41ce56
Yohann Ciurlik <yohann.ciurlik@capgemini.com>
2021-02-19 12:35:54 +0100

    Fix NPM dependencies

--- dist/hello-world.js
+++ dist/hello-world.js
@@ -1,23 +1,6 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global.hw = {})));
-}(this, (function (exports) { 'use strict';
-
-  var index = 42;
-
-  function hello() {
-    console.log('By the way, the answer is ' + index);
-    return 'hello';
-  }
-
-  function world() {
-    return 'world';
-  }
-
-  exports.hello = hello;
-  exports.world = world;
-
-  Object.defineProperty(exports, '__esModule', { value: true });
-
-})));
+(function(g,f){typeof exports==='object'&&typeof module!=='undefined'?f(exports):typeof define==='function'&&define.amd?define(['exports'],f):(g=typeof globalThis!=='undefined'?globalThis:g||self,f(g.hw={}));}(this,(function(exports){'use strict';var index = 42;function hello() {
+  console.log("By the way, the answer is " + index);
+  return 'hello';
+}function world() {
+  return 'world';
+}exports.hello=hello;exports.world=world;Object.defineProperty(exports,'__esModule',{value:true});})));
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,8 +1,12 @@
 
 import pkg from './package.json';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+
+/**
+ * TODO : Check this : https://www.learnwithjason.dev/blog/learn-rollup-js 
+ **/ 
 
 export default {
   input: './src/index.js',
@@ -11,13 +15,16 @@ export default {
     format: 'umd',
     // This is the name of the exported global object.
     // So the world function could be called with hello.world();
-    name: pkg.name
+    name: pkg.name,
+    compact: true
   },
   plugins: [
     resolve(),
     commonjs(),
     babel({
-      exclude: 'node_modules/**'  // only transpile our source code
-    })
+      exclude: 'node_modules/**',  // only transpile our source code
+      babelHelpers: 'external'
+    }),
+    //uglify()
   ]
 }
